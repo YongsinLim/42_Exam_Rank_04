3f0651283337c754e4556bd312b65e5ba9738230
YongsinLim <micky.lys@gmail.com>
2026-04-23 19:05:49 +0800

    Move argo's too long question to next line

---
+++ Level_2/argo/argo.c
@@ -0,0 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   argo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yolim <yolim@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/22 19:33:46 by yolim             #+#    #+#             */
+/*   Updated: 2026/04/22 19:37:03 by yolim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h> // for getc, ungetc, printf, fscanf
+#include <stdlib.c> // for malloc, calloc, realloc, free
+#include <ctype.h> // for isdigit
+#include <unistd.h> // for write
